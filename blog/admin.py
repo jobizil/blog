c2a7a053f8db05ef8fa18e40c9a9c110fd5a4baa
Job Ugbem <ugbemjob57@gmail.com>
2020-11-20 02:24:40 +0100

    Setup Database and data migration

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
-
-
+admin.site.register(Post)
+admin.site.site_title = 'Blog'
+admin.site.site_header = 'Admin Area'
+admin.site.index_title = "Django Blog - Admin"
